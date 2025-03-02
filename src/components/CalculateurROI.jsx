d11d05b3033672066c4225fc915bf85440c262f9
yoprobotics <yoprobotics@gmail.com>
2025-03-02 15:39:47 -0500

    Section 3: Implémentation complète du calcul de ROI

--- src/components/CalculateurROI.jsx
+++ src/components/CalculateurROI.jsx
@@ -170,3 +170,196 @@ const CalculateurROI = () => {
       });
     }
   }, [parametresSystemeAutomatise.capaciteTraitement]);
+  
+  // Fonction de calcul des résultats
+  const calculerROI = () => {
+    const {
+      coutSysteme, coutInstallation, coutIngenierie, coutFormation, formationContinue,
+      coutMaintenance, coutEnergie, dureeVie, tauxAmortissement, coutMainOeuvre, 
+      nbEmployesRemplaces, reductionDechet, coutDechet, augmentationProduction, 
+      reductionEnergie, coutEnergieTonne, reductionEau, coutEauTonne, ameliorationQualite,
+      reductionEmpreinteCO2, capaciteTraitement, tauxRejets, reductionAccidents,
+      supportTechnique, miseANiveauLogicielle, consommablesAnnuels, subventions
+    } = parametresSystemeAutomatise;
+
+    const {
+      capacite, nombreEmployes, maintenance: maintenanceActuelle, 
+      energie: energieActuelle, perteProduction, tauxRejets: tauxRejetsActuel,
+      frequenceAccident, coutMoyenAccident, tempsArretAccident, formationAnnuelle,
+      consommablesAnnuels: consommablesActuels
+    } = parametresSystemeActuel;
+
+    const {
+      margeBrute, tauxInflation, tauxActualisation, productionAnnuelle,
+      heuresOperationParJour, joursOperationParAn, coutQualite, coutRebut
+    } = parametresGeneraux;
+    
+    // Calcul de l'investissement initial
+    const investissementInitial = coutSysteme + coutInstallation + coutIngenierie + coutFormation - subventions;
+    
+    // Initialisation des variables
+    let fluxTresorerie = [];
+    let cumulFluxTresorerie = 0;
+    let valeurActuelleNette = -investissementInitial;
+    let periodeRecuperation = dureeVie;
+    let recuperationAtteinte = false;
+    let totalTonnesCO2Economisees = 0;
+    
+    // Calcul du nombre d'heures d'opération par an
+    const heuresOperationAnnuelles = heuresOperationParJour * joursOperationParAn;
+    
+    // Calcul de la production annuelle (actuelle vs automatisée)
+    const productionActuelle = capacite * heuresOperationAnnuelles * (1 - perteProduction / 100);
+    const productionAutomatisee = capaciteTraitement * heuresOperationAnnuelles;
+    const differenceProductionCalc = productionAutomatisee - productionActuelle;
+    
+    // Calcul des économies d'accidents
+    const economiesAccidents = (frequenceAccident * coutMoyenAccident * reductionAccidents / 100);
+    
+    // Calcul des économies liées au temps d'arrêt dû aux accidents
+    const valeurProductionHoraire = (productionAnnuelle * margeBrute) / heuresOperationAnnuelles;
+    const economiesTempsArretCalc = frequenceAccident * tempsArretAccident * valeurProductionHoraire * reductionAccidents / 100;
+    
+    // Calcul de la réduction de main d'œuvre
+    const reductionMainOeuvreCalc = (nombreEmployes - (nombreEmployes - nbEmployesRemplaces)) * coutMainOeuvre;
+    
+    // Variable pour stocker le bénéfice de qualité de la dernière année (pour l'affichage)
+    let dernierBeneficeQualite = 0;
+    
+    // Calcul des économies annuelles et bénéfices
+    for (let annee = 1; annee <= dureeVie; annee++) {
+      // Calcul des coûts ajustés avec l'inflation
+      const facteurInflation = Math.pow(1 + tauxInflation / 100, annee - 1);
+      const maintenanceAnnuelle = coutMaintenance * facteurInflation;
+      const maintenanceActuelleAjustee = maintenanceActuelle * facteurInflation;
+      const energieOperationAnnuelle = coutEnergie * facteurInflation;
+      const energieActuelleAjustee = energieActuelle * facteurInflation;
+      const formationContinueAnnuelle = formationContinue * facteurInflation;
+      const formationActuelleAjustee = formationAnnuelle * facteurInflation;
+      const supportTechniqueAnnuel = supportTechnique * facteurInflation;
+      const miseANiveauAnnuelle = miseANiveauLogicielle * facteurInflation;
+      const consommablesNouveaux = consommablesAnnuels * facteurInflation;
+      const consommablesActuelsAjustes = consommablesActuels * facteurInflation;
+      
+      // Calcul des économies
+      const economiePersonnel = reductionMainOeuvreCalc * facteurInflation;
+      const economieMaintenance = maintenanceActuelleAjustee - maintenanceAnnuelle;
+      const economieEnergie = energieActuelleAjustee - energieOperationAnnuelle;
+      const economieFormation = formationActuelleAjustee - formationContinueAnnuelle;
+      const economieConsommables = consommablesActuelsAjustes - consommablesNouveaux;
+      
+      // Économies liées à la réduction des déchets
+      const unitesDechetsReduites = (productionAnnuelle * reductionDechet) / 100;
+      const economieDechet = unitesDechetsReduites * coutDechet * facteurInflation;
+      
+      // Économies liées à la réduction des rejets
+      const economieRejets = productionAnnuelle * (tauxRejetsActuel - tauxRejets) / 100 * coutRebut * facteurInflation;
+      
+      // Économies liées à la réduction de consommation d'énergie dans le processus
+      const economieEnergieProcessus = (productionAnnuelle * reductionEnergie / 100) * coutEnergieTonne * facteurInflation;
+      
+      // Économies liées à la réduction de consommation d'eau
+      const economieEau = (productionAnnuelle * reductionEau / 100) * coutEauTonne * facteurInflation;
+      
+      // Bénéfices liés à l'augmentation de la production
+      const productionSupplementaire = productionAnnuelle * (augmentationProduction / 100);
+      const beneficeSupplementaire = productionSupplementaire * margeBrute * facteurInflation;
+      
+      // Bénéfices liés à l'amélioration de la qualité (moins de retours, meilleure réputation)
+      const beneficeQualite = (productionAnnuelle * ameliorationQualite / 100) * (margeBrute * 0.2) * facteurInflation;
+      
+      // Stockage de la dernière valeur pour l'affichage
+      if (annee === dureeVie) {
+        dernierBeneficeQualite = beneficeQualite;
+      }
+      
+      // Économies liées à la sécurité (ajustées pour l'inflation)
+      const economieSecuriteAjustee = economiesAccidents * facteurInflation;
+      const economieTempsArretAjustee = economiesTempsArretCalc * facteurInflation;
+      
+      // Calcul de la réduction des émissions de CO2 (estimation)
+      const tonnesCO2Economisees = (productionAnnuelle * reductionEmpreinteCO2 / 100);
+      totalTonnesCO2Economisees += tonnesCO2Economisees;
+      
+      // Amortissement
+      const amortissement = (investissementInitial / dureeVie) * (tauxAmortissement / 100);
+      
+      // Calcul des coûts cachés
+      const coutsCachesAnnuels = supportTechniqueAnnuel + miseANiveauAnnuelle;
+      
+      // Calcul du flux de trésorerie annuel
+      const fluxAnnuel = economiePersonnel + economieDechet + economieMaintenance + economieEnergie + 
+                       economieEnergieProcessus + economieEau + economieRejets + economieFormation +
+                       economieConsommables + beneficeSupplementaire + beneficeQualite + 
+                       economieSecuriteAjustee + economieTempsArretAjustee - 
+                       maintenanceAnnuelle - energieOperationAnnuelle - coutsCachesAnnuels + amortissement;
+      
+      // Calcul du flux de trésorerie actualisé
+      const facteurActualisation = Math.pow(1 + tauxActualisation / 100, annee);
+      const fluxActualise = fluxAnnuel / facteurActualisation;
+      
+      // Mise à jour de la VAN
+      valeurActuelleNette += fluxActualise;
+      
+      // Calcul du délai de récupération
+      cumulFluxTresorerie += fluxAnnuel;
+      if (cumulFluxTresorerie >= investissementInitial && !recuperationAtteinte) {
+        const cumulPrecedent = cumulFluxTresorerie - fluxAnnuel;
+        const fractionAnnee = (investissementInitial - cumulPrecedent) / fluxAnnuel;
+        periodeRecuperation = annee - 1 + fractionAnnee;
+        recuperationAtteinte = true;
+      }
+      
+      // Ajout des résultats annuels
+      fluxTresorerie.push({
+        annee,
+        fluxAnnuel,
+        fluxActualise,
+        cumulFluxTresorerie,
+        economiePersonnel,
+        economieDechet,
+        economieMaintenance,
+        economieEnergie,
+        economieEnergieProcessus,
+        economieEau,
+        economieFormation,
+        economieConsommables,
+        beneficeSupplementaire,
+        beneficeQualite,
+        economieRejets,
+        economieSecuriteAjustee,
+        economieTempsArretAjustee,
+        maintenanceAnnuelle,
+        energieOperationAnnuelle,
+        coutsCachesAnnuels,
+        amortissement,
+        tonnesCO2Economisees
+      });
+    }
+    
+    // Calcul du ROI
+    const totalBenefices = fluxTresorerie.reduce((sum, item) => sum + item.fluxAnnuel, 0);
+    const roiCalcule = (totalBenefices / investissementInitial) * 100;
+    
+    // Calcul du TRI (approximation simplifiée)
+    let triApprox = calculerTRI(investissementInitial, fluxTresorerie.map(item => item.fluxAnnuel));
+    
+    // Calcul de l'économie annuelle moyenne
+    const economieAnnuelleCalc = totalBenefices / dureeVie;
+    
+    // Mise à jour des résultats
+    setResultats({
+      fluxTresorerie,
+      roi: roiCalcule,
+      delaiRecuperation: periodeRecuperation,
+      van: valeurActuelleNette,
+      tri: triApprox,
+      economiesCO2: totalTonnesCO2Economisees,
+      differenceProduction: differenceProductionCalc,
+      economieAnnuelle: economieAnnuelleCalc,
+      reductionMainOeuvre: reductionMainOeuvreCalc,
+      economiesSecurite: economiesAccidents,
+      economiesQualite: dernierBeneficeQualite,
+      economiesTempsArret: economiesTempsArretCalc
+    });
+  };
