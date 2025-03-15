3ab261336ced1de534aa8fef8debbf9668103ab3
yoprobotics <yoprobotics@gmail.com>
2025-03-15 19:01:29 -0400

    Ajout du composant commun Tooltip

--- src/components/common/Tooltip.jsx
+++ src/components/common/Tooltip.jsx
@@ -1,73 +1,52 @@
 import React, { useState } from 'react';
 
 /**
- * Composant d'infobulle réutilisable
- * @param {Object} props - Propriétés React
- * @returns {JSX.Element} - Infobulle
+ * Composant d'infobulle pour afficher de l'aide ou des informations supplémentaires
+ * @param {Object} props - Propriétés du composant
+ * @param {string|JSX.Element} props.content - Contenu de l'infobulle
+ * @param {string} [props.position='top'] - Position de l'infobulle (top, bottom, left, right)
+ * @returns {JSX.Element} - Composant d'infobulle
  */
-const Tooltip = ({ 
-  children, 
-  content, 
-  position = 'top', 
-  width = 'max-w-xs', 
-  delay = 300,
-  className = ''
-}) => {
+const Tooltip = ({ content, position = 'top' }) => {
   const [isVisible, setIsVisible] = useState(false);
-  const [timeoutId, setTimeoutId] = useState(null);
 
-  // Fonction pour afficher l'infobulle après un délai
-  const showTooltip = () => {
-    const id = setTimeout(() => {
-      setIsVisible(true);
-    }, delay);
-    setTimeoutId(id);
-  };
-
-  // Fonction pour masquer l'infobulle et annuler le délai
-  const hideTooltip = () => {
-    if (timeoutId) {
-      clearTimeout(timeoutId);
-      setTimeoutId(null);
+  // Déterminer la classe de position
+  const getPositionClass = () => {
+    switch (position) {
+      case 'bottom':
+        return 'top-full mt-1';
+      case 'left':
+        return 'right-full mr-1';
+      case 'right':
+        return 'left-full ml-1';
+      case 'top':
+      default:
+        return 'bottom-full mb-1';
     }
-    setIsVisible(false);
-  };
-
-  // Positions possibles de l'infobulle
-  const positions = {
-    top: 'bottom-full left-1/2 transform -translate-x-1/2 mb-2',
-    right: 'left-full top-1/2 transform -translate-y-1/2 ml-2',
-    bottom: 'top-full left-1/2 transform -translate-x-1/2 mt-2',
-    left: 'right-full top-1/2 transform -translate-y-1/2 mr-2',
-  };
-
-  // Position des flèches
-  const arrows = {
-    top: 'top-full left-1/2 transform -translate-x-1/2 border-l-transparent border-r-transparent border-b-transparent',
-    right: 'right-full top-1/2 transform -translate-y-1/2 border-t-transparent border-b-transparent border-r-transparent',
-    bottom: 'bottom-full left-1/2 transform -translate-x-1/2 border-l-transparent border-r-transparent border-t-transparent',
-    left: 'left-full top-1/2 transform -translate-y-1/2 border-t-transparent border-b-transparent border-l-transparent',
   };
 
   return (
-    <div 
-      className={`relative inline-block ${className}`}
-      onMouseEnter={showTooltip}
-      onMouseLeave={hideTooltip}
-      onFocus={showTooltip}
-      onBlur={hideTooltip}
-    >
-      {children}
+    <div className="relative inline-block ml-1">
+      {/* Icône d'information */}
+      <span
+        className="text-gray-400 hover:text-gray-600 cursor-help transition-colors"
+        onMouseEnter={() => setIsVisible(true)}
+        onMouseLeave={() => setIsVisible(false)}
+        onClick={() => setIsVisible(!isVisible)}
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>
+      </span>
       
+      {/* Contenu de l'infobulle */}
       {isVisible && (
-        <div className={`absolute z-20 ${positions[position]} ${width}`}>
-          <div className="bg-gray-800 text-white text-sm rounded-md py-2 px-3 shadow-lg">
-            {content}
-            <div 
-              className={`absolute border-4 border-gray-800 ${arrows[position]}`}
-              style={{ width: '0', height: '0' }}
-            />
-          </div>
+        <div 
+          className={`absolute z-10 bg-gray-800 text-white text-xs rounded py-1 px-2 w-48 ${getPositionClass()}`}
+          style={{ whiteSpace: 'normal' }}
+        >
+          {content}
+          <div className="tooltip-arrow"></div>
         </div>
       )}
     </div>
