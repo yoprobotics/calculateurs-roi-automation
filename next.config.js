93258409ae0900d8c6a77b3763a841cf833d022d
yoprobotics <yoprobotics@gmail.com>
2025-03-15 12:50:01 -0400

    Ajouter configuration Next.js pour le domaine personnalisé

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  trailingSlash: true,
+  basePath: '',
+  // Assurez-vous que ces options sont compatibles avec votre configuration React
+}
+
+module.exports = nextConfig
