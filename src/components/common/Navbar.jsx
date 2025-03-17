69e0feb6c6c527677b49af48b1b6ca5ccfa7a61a
yoprobotics <yoprobotics@gmail.com>
2025-03-16 23:00:02 -0400

    Utilisation d'un SVG inline pour le logo YoProbotics

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -15,16 +15,76 @@ const Navbar = () => {
     window.location.reload();
   };
 
-  // URL absolue du logo sur GitHub
-  const logoUrl = "https://raw.githubusercontent.com/yoprobotics/calculateurs-roi-automation/main/src/logo.png";
-
   return (
     <nav className="bg-white shadow-sm">
       <div className="container mx-auto px-4">
         <div className="flex justify-between h-16">
           {/* Logo et nom - Actualise l'application au clic */}
           <div className="flex items-center cursor-pointer" onClick={refreshApp}>
-            <img src={logoUrl} alt="YoProbotics" className="h-8 w-auto mr-3" />
+            {/* SVG Logo de YoProbotics directement inclus dans le code */}
+            <svg 
+              width="100" 
+              height="40" 
+              viewBox="0 0 400 150" 
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-8 mr-3"
+            >
+              <g>
+                <path 
+                  d="M27,20 L65,20 L65,60 L90,20 L40,100 L65,100 Z" 
+                  fill="#F9DC5C" 
+                  stroke="#636363" 
+                  strokeWidth="5"
+                />
+                <rect 
+                  x="100" 
+                  y="20" 
+                  width="60" 
+                  height="80" 
+                  fill="#F9DC5C" 
+                  stroke="#636363" 
+                  strokeWidth="5"
+                />
+                <path 
+                  d="M170,20 L230,20 L230,60 L170,60 L170,20 Z M170,70 L230,70 L230,100 L200,100 L170,70 Z" 
+                  fill="#F9DC5C" 
+                  stroke="#636363" 
+                  strokeWidth="5"
+                />
+                <path 
+                  d="M240,20 L305,20 L305,35 L270,35 L270,45 L300,45 L300,60 L270,60 L270,85 L305,85 L305,100 L240,100 Z" 
+                  fill="#000000"
+                />
+                <path 
+                  d="M310,20 L345,20 C365,20 375,35 375,60 C375,85 365,100 345,100 L310,100 Z" 
+                  fill="#000000"
+                />
+                <path 
+                  d="M325,35 L340,35 C350,35 355,45 355,60 C355,75 350,85 340,85 L325,85 Z" 
+                  fill="#FFFFFF"
+                />
+                <rect x="380,20" y="20" width="15" height="80" fill="#000000" />
+                <rect x="380,45" y="45" width="30" height="15" fill="#000000" />
+                <path 
+                  d="M420,20 L455,20 L455,85 L475,85 L475,100 L420,100 Z" 
+                  fill="#000000"
+                />
+                <rect x="480,20" y="20" width="15" height="80" fill="#000000" />
+                <path 
+                  d="M505,20 L565,20 L565,35 L550,35 L550,100 L520,100 L520,35 L505,35 Z" 
+                  fill="#000000"
+                />
+                <rect x="570,20" y="20" width="15" height="80" fill="#000000" />
+                <path 
+                  d="M595,35 L620,35 L620,45 L595,45 Z M595,75 L620,75 L620,85 L595,85 Z M595,20 L635,20 L635,100 L595,100 Z" 
+                  fill="#000000"
+                />
+                <path 
+                  d="M640,100 L675,20 L685,20 L720,100 L695,100 L690,85 L670,85 L665,100 Z M675,70 L685,70 L680,55 Z" 
+                  fill="#000000"
+                />
+              </g>
+            </svg>
             <span className="text-xl font-semibold text-gray-800">Calculateurs ROI</span>
           </div>
           
