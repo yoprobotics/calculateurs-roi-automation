db1f59679747b458fd0cddf11db058dec757c9c2
yoprobotics <yoprobotics@gmail.com>
2025-03-17 06:01:47 -0400

    Ajout d'un point d'entrée pour faciliter les importations du calculateur pâtes et papiers

---
+++ src/components/calculateurs/patespapiers/index.js
@@ -0,0 +1,17 @@
+// Point d'entrée pour les exportations du calculateur pâtes et papiers
+
+import CalculateurPatesPapiers from './CalculateurPatesPapiers';
+import ParametresSysteme from './ParametresSysteme';
+import ComparatifSystemes from './ComparatifSystemes';
+import ResultatsFinanciers from './ResultatsFinanciers';
+import ImpactEnvironnemental from './ImpactEnvironnemental';
+
+export {
+  CalculateurPatesPapiers,
+  ParametresSysteme,
+  ComparatifSystemes,
+  ResultatsFinanciers,
+  ImpactEnvironnemental
+};
+
+export default CalculateurPatesPapiers;
