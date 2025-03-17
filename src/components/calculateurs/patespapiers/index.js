3008cb0373e9352acbeb9b3462a6ba2d43bf7b4a
yoprobotics <yoprobotics@gmail.com>
2025-03-17 06:28:36 -0400

    Créer la structure de dossier pour le calculateur de pâtes et papiers

--- src/components/calculateurs/patespapiers/index.js
+++ src/components/calculateurs/patespapiers/index.js
@@ -1,17 +1,3 @@
-// Point d'entrée pour les exportations du calculateur pâtes et papiers
-
 import CalculateurPatesPapiers from './CalculateurPatesPapiers';
-import ParametresSysteme from './ParametresSysteme';
-import ComparatifSystemes from './ComparatifSystemes';
-import ResultatsFinanciers from './ResultatsFinanciers';
-import ImpactEnvironnemental from './ImpactEnvironnemental';
-
-export {
-  CalculateurPatesPapiers,
-  ParametresSysteme,
-  ComparatifSystemes,
-  ResultatsFinanciers,
-  ImpactEnvironnemental
-};
 
 export default CalculateurPatesPapiers;
